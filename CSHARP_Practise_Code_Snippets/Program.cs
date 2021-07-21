3602ee3ab9f62bfac29b5354b2d0917d90b05c7b
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-07-21 10:32:39 +0200

    Added [TABLE] 01

---
+++ CSHARP_Practise_Code_Snippets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSHARP_Practise_Code_Snippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- [TABLE] 01/Program.cs
+++ [TABLE] 01/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 
-namespace _Table__01
+namespace _TABLE__01
 {
-    /* Task:
+    class Program
+    {
+        /* Task:
      * Create a 2-dimensional array with the size of 4x3 and populate it with whatever values you want.
      * Then print the contents of the whiteboard on the screen keeping the layout of the whiteboard (therefore 4
      * rows, 3 columns). Additionally, at the end of the count and print the totals on the screen individual lines. 
@@ -10,10 +12,6 @@ namespace _Table__01
      * Tasks expansion:
      * Adds subtraction, multiplication, division.
      */
-
-
-    class Program
-    {
         static void Main(string[] args)
         {
             //Table
@@ -29,7 +27,7 @@ namespace _Table__01
             Console.WriteLine("Print Table:");
             for (int i = 0; i < tab.GetLength(0); ++i)
             {
-                for(int j = 0; j <tab.GetLength(1); ++j)
+                for (int j = 0; j < tab.GetLength(1); ++j)
                 {
                     Console.Write("{0}", tab[i, j]);
                 }
@@ -38,7 +36,7 @@ namespace _Table__01
             Console.WriteLine();
 
             //Maths 
-            for (int i = 0; i<tab.GetLength(0); ++i)
+            for (int i = 0; i < tab.GetLength(0); ++i)
             {
                 int lineSum = 0;
                 int lineDif = 0;
@@ -57,16 +55,18 @@ namespace _Table__01
                     else
                     {
                         lineQuo /= tab[i, j];
-                    }                    
+                    }
                 }
 
-            Console.WriteLine("Line Sum {0}: {1}", i, lineSum);
-            Console.WriteLine("Line Difference {0}: {1}", i, lineSum);
-            Console.WriteLine("Line Product {0}: {1}", i, lineSum);
-            Console.WriteLine("Line Quotient {0}: {1}", i, lineSum);
-            Console.WriteLine();
+                Console.WriteLine("Line Sum {0}: {1}", i, lineSum);
+                Console.WriteLine("Line Difference {0}: {1}", i, lineSum);
+                Console.WriteLine("Line Product {0}: {1}", i, lineSum);
+                Console.WriteLine("Line Quotient {0}: {1}", i, lineSum);
+                Console.WriteLine();
             }
             Console.ReadKey();//Compilation Break Line
         }
+
+
     }
 }
