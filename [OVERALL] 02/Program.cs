61b874e3b66d9709f67fcbfa06df6500cd38e7c3
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-07-21 13:35:38 +0200

    Added New Folder Structure

---
+++ [OVERALL] 02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _OVERALL__02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
