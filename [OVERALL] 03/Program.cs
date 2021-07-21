cfcd703bb180bbe88aee56ce8d0ea057a870788d
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-07-21 15:00:21 +0200

    Added [OVERALL] 03

---
+++ [OVERALL] 03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _OVERALL__03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get value of n
+            int n;
+            Console.Write("Insert value of n: ");
+            string text = Console.ReadLine();
+            if (!int.TryParse(text, out n)) throw new ArgumentException("n is not an integer!\n");
+
+            // Calculate the factorial n number and print it on the screen.
+            int factorial = Factorial(n);
+            Console.WriteLine("Factorial of number {0} is: {1}", n, factorial);
+            Console.ReadKey();
+        }
+
+        static int Factorial(int factorialToCount)
+        {
+            int i = 1;
+            int result = 1;
+            while (i <= factorialToCount) result *= i++;
+            return result;
+        }
+    }
+}
