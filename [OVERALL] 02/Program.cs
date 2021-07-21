dea13656b152aab25f32d0e598a31fb57bdac3c4
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-07-21 13:47:49 +0200

    Added [OVERALL] 02

--- [OVERALL] 02/Program.cs
+++ [OVERALL] 02/Program.cs
@@ -6,7 +6,28 @@ namespace _OVERALL__02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string aString;
+                int a;
+                string bString;
+                int b;
+
+                //EnterSection
+                Console.WriteLine("Enter First:");
+                aString = Console.ReadLine();
+                Console.WriteLine("Enter Second:");
+                bString = Console.ReadLine();
+                int.TryParse(aString, out a);
+                int.TryParse(bString, out b);
+                Console.WriteLine("Division result: {0}", a / b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You cannot divide by 0");
+            }
+
+            Console.ReadKey();
         }
     }
 }
