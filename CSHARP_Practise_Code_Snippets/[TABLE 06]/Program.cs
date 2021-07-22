8f4aaae2bdc51ad4868e50b70025c2b112763ddd
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-07-22 15:47:57 +0200

    Added [TABLE] 06

---
+++ CSHARP_Practise_Code_Snippets/[TABLE 06]/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _TABLE_06_
+{
+    class Program
+    {
+
+        /*TARGET
+         * 1 2
+         * 3 4 5 6
+         * 7 8 9
+         */
+        static void Main(string[] args)
+        {
+            int[][] tab =
+            {
+                new int[] {1,2},
+                new int[] {3,4,5,6},
+                new int[] {7, 8 ,9}
+            };
+
+            int sum = 0;
+            for(int i = 0; i < tab.Length; ++i)
+            {
+                for (int j = 0; j < tab[i].Length; ++j)
+                {
+                    sum += tab[i][j];
+                }
+
+                Console.WriteLine(sum);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
