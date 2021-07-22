17ebc2dedd95632d760dda1b9d351f58968e6eb3
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-07-22 08:50:25 +0200

    Added [TABLE] 03

---
+++ CSHARP_Practise_Code_Snippets/[TABLE] 03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _TABLE__03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize an array containing four Int arrays
+            int[][] tab = new int[10][];
+            Random rnd = new Random();
+            for (int i = 0; i < tab.Length; ++i){
+                tab[i] = new int[1 + 2 * i];
+                for (int j = 0; j < tab[i].Length; ++j)
+                    tab[i][j] = rnd.Next(0, 10);
+            }
+
+            //Printing
+            for (int i = 0; i < tab.Length; ++i)
+            {
+                for(int j = 0; j<tab[i].Length; ++j)
+                {
+                    Console.Write("{0} ", tab[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
