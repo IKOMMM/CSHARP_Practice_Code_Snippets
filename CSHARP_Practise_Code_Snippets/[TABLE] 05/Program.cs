624e07b937b83545c38a2d6ad2e39e52c49bb637
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-07-22 10:07:10 +0200

    Added [TABLE] 06

---
+++ CSHARP_Practise_Code_Snippets/[TABLE] 05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _TABLE__05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert your text");
+            string textBefore = Console.ReadLine();
+            string textAfter = TrimSpaces(textBefore);
+
+            Console.WriteLine("Your text before: {0}", textBefore);
+            Console.WriteLine("Your text after: {0}", textAfter);
+            Console.ReadKey();
+
+        }
+
+        static string TrimSpaces(string text)
+        {
+            //Target string, to hold a shortened version of the text
+            string textWithoutSpaces = "";
+
+            for(int i =0; i<text.Length; ++i)
+            {
+                // If the character is not empty, add it to the target string
+                if(text[i]!= ' ' && text[i] != '\t' && text[i] != 'n')
+                    textWithoutSpaces += text[i];
+            }
+
+            return textWithoutSpaces;
+        }
+    }
+}
