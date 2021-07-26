0d073a1d367c6ac81a83c11ddae5260e24bf43c5
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-07-26 09:03:04 +0200

    Added [OVERALL] 05

---
+++ CSHARP_Practise_Code_Snippets/[OVERALL] 05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _OVERALL__05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string xString;
+            Console.WriteLine("Enter elements of the fibonacci sequence: ");
+            xString = Console.ReadLine();
+            int.TryParse(xString, out x);
+
+            //Print
+            for (int i = 0; i < x; ++i)
+                Console.WriteLine(Fibonacci(i));
+            Console.ReadKey();
+        }
+
+        static long Fibonacci(int n)
+        {
+            //F0 = 0, F1 = 1
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            // In other cases, call the function twice
+            // recursively to obtain the element values of F(n-1) + F(n-2)
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
