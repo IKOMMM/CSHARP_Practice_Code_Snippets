0df7589c9b8419435bbea492ae6c83ee1fc0c5cf
IKOMMM <45951520+IKOMMM@users.noreply.github.com>
2021-08-06 11:12:32 +0200

    Added [OOP] 03

---
+++ CSHARP_Practise_Code_Snippets/[OOP] 03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _OOP__03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dogA = new Dog();
+            dogA.Sound = ("Wof");
+            
+            Dog dogB = new Dog();
+            dogB.Sound = ("Woof Woof");
+
+            dogA.MakeSound();
+            dogB.MakeSound();
+            Console.ReadKey();
+        }
+    }
+
+    #region Interfaces
+    interface IMakingSound
+    {
+        void MakeSound();
+    }
+
+    interface ILand
+    {
+        void HowManyLegs();
+    }
+    #endregion
+
+    class Dog : IMakingSound, ILand
+    {
+        public string Sound { get; set; }
+        public void MakeSound()
+        {
+            Console.WriteLine(Sound);
+        }
+        public void HowManyLegs()
+        {
+            Console.WriteLine("Dog Has 4 legs");
+        }
+    }
+}
